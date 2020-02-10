6cb8d475675a022a68cead0a008cd45ee7f17b7c
Pieter Steyn <pieters@roam.africa>
2020-02-10 22:45:39 +0200

    Adjust according to latest JsonFormatter class.

--- src/DataDogFormatter.php
+++ src/DataDogFormatter.php
@@ -6,12 +6,6 @@ use DateTime;
 use Monolog\Formatter\JsonFormatter;
 
 /**
- * Class DataDogFormatter
- *
- * @package   Myli\DatadogLogger
- * @author    Aurélien SCHILTZ <aurelien@myli.io>
- * @copyright 2016-2019 Myli
- * @license   http://www.opensource.org/licenses/mit-license.html  MIT License
  */
 class DataDogFormatter extends JsonFormatter
 {
@@ -19,34 +13,14 @@ class DataDogFormatter extends JsonFormatter
     const LARAVEL_LOG_DATETIME_KEY = 'datetime';
 
     /**
-     * Appends every variable needed by DataDog
-     *
-     * @TODO Handle trace_id for projects linked with DataDog APM
-     *
-     * @param array $record
-     *
-     * @return string
-     * @see  \Monolog\Formatter\JsonFormatter::format()
-     * @see  https://docs.datadoghq.com/logs/processing/#reserved-attributes
+     * simply copy level_name to status
      */
-    public function format(array $record)
+    public function format(array $record): string
     {
-        if (isset($record[self::LARAVEL_LOG_DATETIME_KEY]) &&
-            ($record[self::LARAVEL_LOG_DATETIME_KEY] instanceof DateTime)) {
-            /**
-             * @var DateTime $dateTimeObj
-             */
-            $dateTimeObj              = $record[self::LARAVEL_LOG_DATETIME_KEY];
-            $record['published_date'] = $dateTimeObj->format(DateTime::ISO8601);
-        }
 
         if (isset($record['level_name'])) {
             $record['status'] = $record['level_name'];
         }
-        $record['ddsource'] = 'php-' . php_sapi_name();
-        $record['source']   = 'php-' . php_sapi_name();
-        $record['service']  = config('app.name');
-        $record['hostname'] = gethostname();
 
         return parent::format($record);
     }
