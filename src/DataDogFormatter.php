e4a1a5558780a591e5a92b7cbbb86b612c56c5de
Pieter Steyn <pieters@roam.africa>
2020-02-11 02:05:33 +0200

    Try add as attributes.

--- src/DataDogFormatter.php
+++ src/DataDogFormatter.php
@@ -25,8 +25,8 @@ class DataDogFormatter extends JsonFormatter
                 $span->getTraceId(),
                 $span->getSpanId()
             );
-            $record['trace_id'] = $span->getTraceId();
-            $record['span_id'] = $span->getSpanId();
+            $record['dd.trace_id'] = $span->getTraceId();
+            $record['dd.span_id'] = $span->getSpanId();
         }
 
         if (isset($record['level_name'])) {
