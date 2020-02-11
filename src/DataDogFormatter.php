ed1388d74f9d29c68a4883d5e1a28f19c9b0dff7
Pieter Steyn <pieters@roam.africa>
2020-02-11 12:06:34 +0200

    Restore spans/traceid

--- src/DataDogFormatter.php
+++ src/DataDogFormatter.php
@@ -14,16 +14,19 @@ class DataDogFormatter extends JsonFormatter
     const LARAVEL_LOG_DATETIME_KEY = 'datetime';
 
     /**
-     * simply copy level_name to status
+     * simply copy level_name to status as datadog expectations
+     * insert the trace_id and span_id
      */
     public function format(array $record): string
     {
         $span = GlobalTracer::get()->getActiveSpan();
         if (null !== $span) {
             $record['message'] .= sprintf(
-                ' [dd.trace_id:%d dd.span_id:%d]',
+                ' [dd.trace_id=%d dd.span_id=%d]',
                 $span->getTraceId(),
                 $span->getSpanId()
+                $record['dd.trace_id'] =  $span->getTraceId();;
+                $record['dd.span_id'] =  $span->getSpanId();;
             );
         }
 
